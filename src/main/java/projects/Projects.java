f5a97d770cfcca206403788e58f12cac671abf73
rd1218 <dfjolguin@outlook.com>
2023-06-17 21:55:15 -0700

    first commit

---
+++ src/main/java/projects/Projects.java
@@ -0,0 +1,12 @@
+package projects;
+
+import java.sql.Connection;
+import projects.dao.DbConnection;
+
+public class Projects {
+
+	public static void main(String[] args) {
+		Connection conn = DbConnection.getConnection();
+	}
+
+}
